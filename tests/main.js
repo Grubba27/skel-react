7012259bf992d3f8e8f71efb30b379f90b78f350
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-06-29 11:49:54 -0300

    updated names to be match with other skels

--- tests/main.js
+++ tests/main.js
@@ -1,9 +1,9 @@
 import assert from "assert";
 
-describe("meteor-react", function () {
+describe("meteor-app", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    assert.strictEqual(name, "test-git9");
+    assert.strictEqual(name, "meteor-app");
   });
 
   if (Meteor.isClient) {
